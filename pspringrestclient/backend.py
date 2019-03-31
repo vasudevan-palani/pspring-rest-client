13c168ce4580923448a6b564a634d5c1d599b324
Palani <vasudevan.palani@gmail.com>
2019-03-31 10:35:27 -0400

    added logging

--- pspringrestclient/backend.py
+++ pspringrestclient/backend.py
@@ -2,6 +2,10 @@ import requests
 
 from pspring import *
 
+import logging
+
+logger = logging.getLogger("pspring-rest-client")
+
 class Backend():
     def __init__(self,*args,**kargs):
         self.url = kargs.get("url")
@@ -18,13 +22,50 @@ class Backend():
         def send(*args,**kargs):
             selfOrig = args[0]
             selfOrig.finalize()
-            
+
+            logger.info({
+                "message" : "request details",
+                "method" : kargs.get("method"),
+                "url" : kargs.get("url"),
+                "data" : kargs.get("data"),
+                "proxies" : kargs.get("proxies"),
+                "headers" : selfOrig.headers
+            })
+
             response = requests.request(
                 kargs.get("method"),
                 kargs.get("url"),
                 headers = selfOrig.headers,
                 data=kargs.get("data"),
                 proxies=kargs.get("proxies"))
+
+            try:
+                responseJson = json.loads(response.text)
+                logger.info({
+                    "message" : "response details",
+                    "method" : kargs.get("method"),
+                    "url" : kargs.get("url"),
+                    "data" : kargs.get("data"),
+                    "proxies" : kargs.get("proxies"),
+                    "headers" : selfOrig.headers,
+                    "status_code" : response.status_code,
+                    "responseHeaders" : response.headers,
+                    "response" : responseJson,
+                    "elapsed" : response.elapsed.total_seconds()
+                })
+            except Exception:
+                logger.info({
+                    "message" : "response details",
+                    "method" : kargs.get("method"),
+                    "url" : kargs.get("url"),
+                    "data" : kargs.get("data"),
+                    "proxies" : kargs.get("proxies"),
+                    "headers" : selfOrig.headers,
+                    "status_code" : response.status_code,
+                    "responseHeaders" : response.headers,
+                    "response" : response.text,
+                    "elapsed" : response.elapsed.total_seconds()
+                })
 
             if response.ok:
                 return response.json()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pspring-rest-client',
-    version='0.0.3',
+    version='0.0.4',
     license='TBD',
     author='Vasudevan Palani',
     author_email='vasudevan.palani@gmail.com',
