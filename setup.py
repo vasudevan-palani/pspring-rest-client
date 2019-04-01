e90ab3a281a9a78f27d5ac4f4293de7a330e53b8
Palani <vasudevan.palani@gmail.com>
2019-04-01 14:42:00 -0400

    updated long desc

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='pspring-rest-client',
-    version='0.0.9',
+    version='0.0.10',
     license='TBD',
     author='Vasudevan Palani',
     author_email='vasudevan.palani@gmail.com',
     url='https://github.com/vasudevan-palani/pspring-rest-client',
-    long_description="README.md",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['pspringrestclient'],
     install_requires=["pspring>=0.0.1"],
     include_package_data=True,
